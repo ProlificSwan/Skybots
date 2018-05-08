46691d12916126ce2cb24c0aa9b6c12a2889ae78
Michael Swan <rmichaelswan@gmail.com>
2018-05-08 00:35:31 -0700

    Removed some fluff, answered question, and fixed blue motor pin numbers.

--- RoboticsGUI/GUI/ViewModel/MainWindowViewModel.cs
+++ RoboticsGUI/GUI/ViewModel/MainWindowViewModel.cs
@@ -36,11 +36,17 @@ namespace Robotics.GUI.ViewModel
 
         public CountdownModel Countdown { get; } = new CountdownModel(TimeSpan.FromSeconds(150));
 
-        //TODO - get LED pin numbers from some standard constants file instead of hardcoding here.
-        //Research: any issue with using static in this case? Most of the variables in this window should technically only be created once...
-        public static TeamDataModel Team1 { get; } = new TeamDataModel("Team 1",22,24,26,28,30,32,2,3);
-        public static TeamDataModel Team2 { get; } = new TeamDataModel("Team 2",23,25,27,29,31,33,2,3);
 
+        /* Question: any issue with using 'static' in this case? Most of the variables in this window should technically only be created once...
+         * Answer: Proper usage: probably only use static if this variable would be shared across all instances of MainWindowViewModel.
+         * This consideration is probably true for the ArduinoModel, but not the TeamDataModel.  If we want to be technically correct,
+         * we should probably make a MainWindowViewModel constructor so that the TeamDataModel and ArduinoModels can be properly 
+         * instantiated in order. That said, there's no reason to have multiple MainWindows right now, so this distinction is currently 
+         * not important. Static works as is right now because the static objects are created in order of declaration (non-static fields may be created in any order).
+         */
+        //TODO - should probably get LED pin numbers from some standard constants class or enum instead of hardcoding here.
+        public static TeamDataModel Team1 { get; } = new TeamDataModel("Team 1", 22, 24,26,28,30,32,2,3); //assumption: this is the red team
+        public static TeamDataModel Team2 { get; } = new TeamDataModel("Team 2",23,25,27,29,31,33,4,5); //assumption: this is the blue team
         //TODO Research: does this need to be public?
         public static ArduinoModel arduino = new ArduinoModel(Team1,Team2);        
 
@@ -59,14 +65,6 @@ namespace Robotics.GUI.ViewModel
         public RelayCommand T1Obs1Decr => _T1Obs1ScoreDecr ?? (_T1Obs1ScoreDecr = new RelayCommand(execute => { Team1.TeamScore.Obstacle1.Decrement(); }, canExecute => { return !Team1.TeamScoringMethod.Obstacle1.IsEnabled; }));
         public RelayCommand T1Obs2Incr => _T1Obs2ScoreIncr ?? (_T1Obs2ScoreIncr = new RelayCommand(execute => { Team1.TeamScore.Obstacle2.Increment(); }, canExecute => { return !Team1.TeamScoringMethod.Obstacle2.IsEnabled; }));
         public RelayCommand T1Obs2Decr => _T1Obs2ScoreDecr ?? (_T1Obs2ScoreDecr = new RelayCommand(execute => { Team1.TeamScore.Obstacle2.Decrement(); }, canExecute => { return !Team1.TeamScoringMethod.Obstacle2.IsEnabled; }));
-        //T1 LED and motor control
-        /*
-        public RelayCommand T1HoverLed => _T1HoverLed ?? (_T1HoverLed = new RelayCommand(execute => { arduino.SetLed(Team1.TeamControl.HoverLed); }, canExecute => { return arduino.ComOK; }));
-        public RelayCommand T1Plat1Led => _T1Plat1Led ?? (_T1Plat1Led = new RelayCommand(execute => { arduino.SetLed(Team1.TeamControl.Platform1Led); }, canExecute => { return arduino.ComOK; }));
-        public RelayCommand T1Plat2Led => _T1Plat2Led ?? (_T1Plat1Led = new RelayCommand(execute => { arduino.SetLed(Team1.TeamControl.Platform2Led); }, canExecute => { return arduino.ComOK; }));
-        public RelayCommand T1Obs1Led => _T1Obs1Led ?? (_T1Obs1Led = new RelayCommand(execute => { arduino.SetLed(Team1.TeamControl.Obstacle1Led); }, canExecute => { return arduino.ComOK; }));
-        public RelayCommand T1Obs2Led => _T1Obs2Led ?? (_T1Obs1Led = new RelayCommand(execute => { arduino.SetLed(Team1.TeamControl.Obstacle2Led); }, canExecute => { return arduino.ComOK; }));
-        public RelayCommand T1StartLed => _T1StartLed ?? (_T1StartLed = new RelayCommand(execute => { arduino.SetLed(Team1.TeamControl.StartLed); }, canExecute => { return arduino.ComOK; }));*/
 
         //T2 score and sensor control
         public RelayCommand T2HoverIncr => _T2HoverScoreIncr ?? (_T2HoverScoreIncr = new RelayCommand(execute => { Team2.TeamScore.Hover.Increment(); }, canExecute => { return !Team2.TeamScoringMethod.Hover.IsEnabled; }));
@@ -79,13 +77,6 @@ namespace Robotics.GUI.ViewModel
         public RelayCommand T2Obs1Decr => _T2Obs1ScoreDecr ?? (_T2Obs1ScoreDecr = new RelayCommand(execute => { Team2.TeamScore.Obstacle1.Decrement(); }, canExecute => { return !Team2.TeamScoringMethod.Obstacle1.IsEnabled; }));
         public RelayCommand T2Obs2Incr => _T2Obs2ScoreIncr ?? (_T2Obs2ScoreIncr = new RelayCommand(execute => { Team2.TeamScore.Obstacle2.Increment(); }, canExecute => { return !Team2.TeamScoringMethod.Obstacle2.IsEnabled; }));
         public RelayCommand T2Obs2Decr => _T2Obs2ScoreDecr ?? (_T2Obs2ScoreDecr = new RelayCommand(execute => { Team2.TeamScore.Obstacle2.Decrement(); }, canExecute => { return !Team2.TeamScoringMethod.Obstacle2.IsEnabled; }));
-        //T2 LED and motor control
-        /*public RelayCommand T2HoverLed => _T2HoverLed ?? (_T2HoverLed = new RelayCommand(execute => { arduino.SetLed(Team2.TeamControl.HoverLed); }, canExecute => { return arduino.ComOK; }));
-        public RelayCommand T2Plat1Led => _T2Plat1Led ?? (_T2Plat1Led = new RelayCommand(execute => { arduino.SetLed(Team2.TeamControl.Platform1Led); }, canExecute => { return arduino.ComOK; }));
-        public RelayCommand T2Plat2Led => _T2Plat2Led ?? (_T2Plat1Led = new RelayCommand(execute => { arduino.SetLed(Team2.TeamControl.Platform2Led); }, canExecute => { return arduino.ComOK; }));
-        public RelayCommand T2Obs1Led => _T2Obs1Led ?? (_T2Obs1Led = new RelayCommand(execute => { arduino.SetLed(Team2.TeamControl.Obstacle1Led); }, canExecute => { return arduino.ComOK; }));
-        public RelayCommand T2Obs2Led => _T2Obs2Led ?? (_T2Obs1Led = new RelayCommand(execute => { arduino.SetLed(Team2.TeamControl.Obstacle2Led); }, canExecute => { return arduino.ComOK; }));
-        public RelayCommand T2StartLed => _T2StartLed ?? (_T2StartLed = new RelayCommand(execute => { arduino.SetLed(Team2.TeamControl.StartLed); }, canExecute => { return arduino.ComOK; }));*/
 
         public override void OnClosing(object sender, CancelEventArgs e)
         {
