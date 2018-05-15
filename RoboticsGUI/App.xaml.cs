f82ddf55d4af2e5b934d818cac9ff3b436ea21c6
Caleb <jcalebc55@gmail.com>
2018-05-14 19:31:36 -0700

    Test commit for new branch, Additional UI screen

--- RoboticsGUI/App.xaml.cs
+++ RoboticsGUI/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using Robotics.GUI.View;
+using System.Windows;
 
 namespace Robotics
 {
@@ -7,5 +8,12 @@ namespace Robotics
     /// </summary>
     public partial class App : Application
     {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            MatchUpsWindow window = new MatchUpsWindow();
+            window.Top = 100;
+            window.Left = 100;
+            window.Show();
+        }
     }
 }
